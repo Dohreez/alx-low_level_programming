a9bdb1cd337a8a8754524e7ed84ffb6c46485f23
Dohreez <dorismbonu23@gmail.com>
2022-11-02 21:58:30 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,12 +24,8 @@ int squareroot(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-	int i = 0;
-
 	if (n < 0)
 		return (-1);
-	if (n == 1)
-		return (1);
 	else
 		return (squareroot(n, (n + 1) / 2));
 }
