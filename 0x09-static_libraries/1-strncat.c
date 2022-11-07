855f01a4c69b6a0acff3e9636ca3b37da5de4290
Dohreez <dorismbonu23@gmail.com>
2022-11-07 13:33:41 -0800

    libmy.a

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that Concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: The number of bytes from src to be appended to dest.
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i, co;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (co = 0; co < n; co++)
+	{
+		dest[i + co] = src[co];
+		if (src[co] == '\0')
+			co = n;
+	}
+
+	return (dest);
+}
+
