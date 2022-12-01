d2570b3e48d9c2c4524ec12458c95e57b26a64f8
Dohreez <dorismbonu23@gmail.com>
2022-12-01 11:21:19 -0800

    2-get_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+
+
+/**
+ *
+ *  * set_bit - Sets the value of a bit at a given index to 1.
+ *
+ *   * @n: A pointer to the bit.
+ *
+ *    * @index: The index to set the value at - indices start at 0.
+ *
+ *     *
+ *
+ *      * Return:
